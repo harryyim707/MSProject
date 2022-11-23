6a2323a34fffd4d951f915a85f4072922bb25d00
Hyunjun Harry Yim <harryyim707@gmail.com>
2022-11-23 23:40:10 +0900

    modified bottom navigation bar from fragments into activities and applied click event on add buttons

---
+++ app/src/main/java/com/example/dietapp/Analysis.java
@@ -0,0 +1,39 @@
+package com.example.dietapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class Analysis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_analysis);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.menu_bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.menu_analysis);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_analysis:
+                        return true;
+                    case R.id.menu_home:
+                        startActivity(new Intent(Analysis.this, MainActivity.class));
+                        return true;
+                    case R.id.menu_calendar:
+                        startActivity(new Intent(Analysis.this, Calendar.class));
+                        return true;
+                }
+                return false;
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/dietapp/MainActivity.java
+++ app/src/main/java/com/example/dietapp/MainActivity.java
@@ -2,68 +2,84 @@ package com.example.dietapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private FragmentManager fragmentManager = getSupportFragmentManager();
-    private MainMenuAnalisysFragment fragmentAnalysis = new MainMenuAnalisysFragment();
-    private MainMenuHomeFragment fragmentHome = new MainMenuHomeFragment();
-    private MainMenuCalendarFragment fragmentCalendar = new MainMenuCalendarFragment();
+    private View view;
+    private ImageButton addBtn1;
+    private ImageButton addBtn2;
+    private ImageButton addBtn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.menu_frame_layout, fragmentAnalysis).commitAllowingStateLoss();
+        //Navigation Bar
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.menu_bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.menu_home);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_analysis:
+                        startActivity(new Intent(MainActivity.this, Analysis.class));
+                        return true;
+                    case R.id.menu_home:
+                        return true;
+                    case R.id.menu_calendar:
+                        startActivity(new Intent(MainActivity.this, Calendar.class));
+                        return true;
+                }
+                return false;
+            }
+        });
+        //apply click event on buttons
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.menu_bottom_navigation);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListner());
+        addBtn1 = findViewById(R.id.addBtn1);
+        addBtn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, food_data_input.class);
+                startActivity(intent);
+            }
+        });
+        addBtn2 = findViewById(R.id.addBtn2);
+        addBtn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, food_data_input.class);
+                startActivity(intent);
+            }
+        });
+        addBtn3 = findViewById(R.id.addBtn3);
+        addBtn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, food_data_input.class);
+                startActivity(intent);
+            }
+        });
+
     }
 
-    class ItemSelectedListner implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private void calculate() {
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-            FragmentTransaction transaction = fragmentManager.beginTransaction();
+    }
 
-            switch (menuItem.getItemId()) {
-                case R.id.menu_analysis:
-                    transaction.replace(R.id.menu_frame_layout, fragmentAnalysis).commitAllowingStateLoss();
-                    break;
-                case R.id.menu_home:
-                    transaction.replace(R.id.menu_frame_layout, fragmentHome).commitAllowingStateLoss();
-                    break;
-                case R.id.menu_calendar:
-                    transaction.replace(R.id.menu_frame_layout, fragmentCalendar).commitAllowingStateLoss();
-                    break;
-            }
-            return true;
-        }
-
-
-        private void calculate() {
-
-        }
-
-        private void summary() {
-            TextView cal = (TextView) findViewById(R.id.cal_val);
-            TextView carbo = (TextView) findViewById(R.id.car_val);
-            TextView prot = (TextView) findViewById(R.id.pro_val);
-            TextView fat = (TextView) findViewById(R.id.fat_val);
-        }
+    private void summary() {
+        TextView cal = (TextView) findViewById(R.id.cal_val);
+        TextView carbo = (TextView) findViewById(R.id.car_val);
+        TextView prot = (TextView) findViewById(R.id.pro_val);
+        TextView fat = (TextView) findViewById(R.id.fat_val);
     }
 }
